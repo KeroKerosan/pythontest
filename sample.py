1468c6958945e3c00bffe2512fa8bb47b26944fd
KeroKerosan <159400892+KeroKerosan@users.noreply.github.com>
2024-02-08 14:32:58 +0900

    Update sample.py

--- sample.py
+++ sample.py
@@ -2,8 +2,8 @@
 import numpy as np
 
 # aとbという名前のNumPy配列を作成し、要素としてaに「1、2」、bに「3、4」を設定します。
-a = np.array([3, 6])
-b = np.array([7, 4])
+a = np.array([1, 2])
+b = np.array([3, 4])
 
 # aとbの加算結果を出力します。
 # aには1と3が足され、bには2と4が足されます。
