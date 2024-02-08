a3ff051d503a4c566d6b84aff19789e7960e488a
KeroKerosan <159400892+KeroKerosan@users.noreply.github.com>
2024-02-08 14:21:20 +0900

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,5 +1,9 @@
+# NumPyライブラリをインポートし、npというエイリアスを設定します。
 import numpy as np
 
+# aとbという名前のNumPy配列を作成し、要素としてaに「1、2」、bに「3、4」を設定します。
 a = np.array([1, 2])
 b = np.array([3, 4])
+
+# aとbの加算結果を出力します。
 print(a + b)
